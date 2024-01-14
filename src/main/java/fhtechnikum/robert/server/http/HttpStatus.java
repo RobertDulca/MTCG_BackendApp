657940d695d888f5676320ec9ffdc07e8c27528d
RobertDulca <dpr200408@gmail.com>
2024-01-14 15:39:41 +0100

    - Dtos are being created automatically through openapi.yml. - HTTPStatus options added

--- src/main/java/fhtechnikum/robert/server/http/HttpStatus.java
+++ src/main/java/fhtechnikum/robert/server/http/HttpStatus.java
@@ -3,8 +3,14 @@ package fhtechnikum.robert.server.http;
 // THOUGHT: Add new relevant status (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
 public enum HttpStatus {
     OK(200, "OK"),
+    CREATED(201, "Created"),
+    NO_CONTENT(204, "No Content"),
     BAD_REQUEST(400, "Bad Request"),
-    NOT_FOUND(404, "Not Found");
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not found"),
+    CONFLICT(409, "Conflict"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private final int code;
     private final String message;
